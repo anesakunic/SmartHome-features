079fdd7d4cc77667151e76928ab76a402f33dfa1
anesakunic <kunicanesa@icloud.com>
2025-02-09 21:59:23 +0100

    third commit - final version

--- SmartHome/Program.cs
+++ SmartHome/Program.cs
@@ -5,35 +5,112 @@
 using System;
 using System.Collections.Generic;
 
-abstract class smartDevice
+abstract class smartDevice      //my base class
 {
     public string Ime { get; protected set;}
-
+    public bool Ukljuceno { get; protected set;} 
 
     protected smartDevice(string ime)   // konstruktor
     {
 
         Ime=ime;
+        Ukljuceno = false;
     }
 
 
 
-    public abstract void Ukljuci();
+    public abstract void Ukljuci();  //abstraktne metode za ukljucivanje i iskljucivanje
     public abstract void Iskljuci();
 
 
+}
+interface Podesavanja
+{
+    void povecavanje();
+    void smanjivanje();
 }
 
 
 
 class smartLighting : smartDevice
 {
+    public int Osvjetljivanje { get; private set; }  
+    public smartLighting(string ime) : base(ime) { Osvjetljivanje =65;}   //konstr
+
+    public override void Ukljuci()
+    {
+        Ukljuceno = true;
+        Console.WriteLine($"Svjetlo {Ime} je sada ON");
+
+    }
+    public override void Iskljuci()
+    {
+        Ukljuceno = false;
+        Console.WriteLine($"Svjetlo {Ime} je sada OFF");
+        
+    }
 
 }
 
 
-class termostat : smartDevice
+class smartTermostat : smartDevice, Podesavanja
 {
+    public int temp { get; private set; }
+    public smartTermostat(string ime) : base(ime) { temp = 24; }
 
+    public override void Ukljuci()
+    {
+        Ukljuceno=true;
+        Console.WriteLine($"Termostat {Ime} je sada ON");
+    }
+    public override void Iskljuci()
+    {
+        Ukljuceno=false;
+        Console.WriteLine($"Termostat {Ime} je sada OFF");
+    }
+
+    public void povecavanje()
+    {
+        temp++;
+       
+        Console.WriteLine($"Temperatura {Ime} je sada povećana na {temp}°C");
     
+    }
+
+    public void smanjivanje()
+    {
+        temp--;
+        Console.WriteLine($"Temperatura {Ime} je sada smanjena na {temp}°C");
+    }
+
+    class Program   //glavna klasa
+    {
+        static void Main()
+        {
+            List<smartDevice> devices = new List<smartDevice>   //lista pametnih uredjaja
+            {
+                new smartLighting("Dnevni Boravak"),
+                new smartTermostat("Dječija soba")
+
+            };
+
+            foreach (var device in devices )
+            {
+                device.Ukljuci();
+            }
+
+            smartTermostat termostat = devices[1] as smartTermostat;
+            termostat?.povecavanje();   //povecavam temp
+            termostat?.smanjivanje();   
+
+            foreach (var device in devices)   //iskljuci sve
+            {
+                device.Iskljuci();
+            }
+        }
+        
+    }
+
+
+
 }
--- SmartHome/obj/Debug/net9.0/SmartHome.AssemblyInfo.cs
+++ SmartHome/obj/Debug/net9.0/SmartHome.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartHome")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c46f07ef041dbb170bcb3f41ab78af85e7c8745")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d04f958a56a4a87773f98d683d90d8ed55046a78")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartHome")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartHome")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
