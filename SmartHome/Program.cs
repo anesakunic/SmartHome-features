d04f958a56a4a87773f98d683d90d8ed55046a78
anesakunic <kunicanesa@icloud.com>
2025-02-09 01:09:29 +0100

    second  commit -classes

--- SmartHome/Program.cs
+++ SmartHome/Program.cs
@@ -2,3 +2,38 @@
 
 // Application of OOP principles 
 
+using System;
+using System.Collections.Generic;
+
+abstract class smartDevice
+{
+    public string Ime { get; protected set;}
+
+
+    protected smartDevice(string ime)   // konstruktor
+    {
+
+        Ime=ime;
+    }
+
+
+
+    public abstract void Ukljuci();
+    public abstract void Iskljuci();
+
+
+}
+
+
+
+class smartLighting : smartDevice
+{
+
+}
+
+
+class termostat : smartDevice
+{
+
+    
+}
--- SmartHome/obj/Debug/net9.0/SmartHome.AssemblyInfo.cs
+++ SmartHome/obj/Debug/net9.0/SmartHome.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartHome")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b4efa71cdad0e69c1513a4d6be1ea4b9ea9862")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c46f07ef041dbb170bcb3f41ab78af85e7c8745")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartHome")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartHome")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
